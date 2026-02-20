9c59dd0c2a3b0cb27ef6c819017251d7b63ac024
Emil Burzo <contact@emilburzo.com>
2026-02-20 17:22:36 +0200

    adjust db connection parameters

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,14 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL || "postgres://postgres@localhost:5432/xcontest";
 
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
+// TODO look into connection pooling
+const client = postgres(connectionString, {
+    max: 5, // Max number of connections
+    idle_timeout: 20, // Max idle time in seconds before closing a connection
+    connect_timeout: 2, // Max time in seconds to wait for a connection
+});
+
+export const db = drizzle(client, {schema});
